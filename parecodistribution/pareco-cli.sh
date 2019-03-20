210cdd1d808b5662baf0026568932e5ea825941d
Antonio Tomac <antonio.tomac@mediatoolkit.com>
2019-03-20 14:01:29 +0100

    enable killing container using CTRL+C

--- parecodistribution/pareco-cli.sh
+++ parecodistribution/pareco-cli.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR=$(dirname "$0")
-java -jar "$DIR/parecoclient/pareco-client-app.jar" "$@"
+exec java -jar "$DIR/parecoclient/pareco-client-app.jar" "$@"
--- parecodistribution/pareco-server.sh
+++ parecodistribution/pareco-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR=$(dirname "$0")
-java -jar "$DIR/parecoserver/pareco-server-app.jar" "$@"
+exec java -jar "$DIR/parecoserver/pareco-server-app.jar" "$@"
