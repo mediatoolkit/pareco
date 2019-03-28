3911b2c5799109062d9243270ab43691251b314e
Antonio Tomac <antonio.tomac@mediatoolkit.com>
2019-03-28 07:30:22 +0100

    document that command lina args wil lock web UI form

--- parecoclientrunner/src/main/java/com/mediatoolkit/pareco/commandline/RunnerCommandLineOptions.java
+++ parecoclientrunner/src/main/java/com/mediatoolkit/pareco/commandline/RunnerCommandLineOptions.java
@@ -19,24 +19,36 @@ import lombok.NoArgsConstructor;
 @Parameters(separators = " =")
 public class RunnerCommandLineOptions implements CommandLineOptions {
 
-	@Parameter(names = {"-p", "--port"}, description = "Port to listen on for incoming http requests, port=0 means random available", order = 1)
+	@Parameter(names = {"-p", "--port"}, description = "Port to listen on for web UI, port=0 means random available", order = 1)
 	private int port = 0;
 
 	@Parameter(
 		names = {"-s", "--server"},
-		description = "Server url, example: http://my.server.com:8080",
+		description = "Server url, example: http://my.server.com:8080, if this parameter is set UI will have it locked",
 		converter = ServerInfoConverter.class,
 		order = 2
 	)
 	private ServerInfo server;
 
-	@Parameter(names = {"-a", "--authToken"}, description = "Authentication token to use to authorize against server", order = 2)
+	@Parameter(
+		names = {"-a", "--authToken"},
+		description = "Authentication token to use to authorize against server, if this parameter is set UI will have it locked",
+		order = 2
+	)
 	private String authToken;
 
-	@Parameter(names = {"-l", "--localDir"}, description = "Local directory to upload from or to download into", order = 3)
+	@Parameter(
+		names = {"-l", "--localDir"},
+		description = "Local directory to upload from or to download into, , if this parameter is set UI will have it locked",
+		order = 3
+	)
 	private String localDir;
 
-	@Parameter(names = {"-r", "--remoteDir"}, description = "Remote directory to download from or to upload into", order = 3)
+	@Parameter(
+		names = {"-r", "--remoteDir"},
+		description = "Remote directory to download from or to upload into, if this parameter is set UI will have it locked",
+		order = 3
+	)
 	private String remoteDir;
 
 	@Parameter(names = {"-h", "--help"}, help = true, description = "Print this help with parameters", order = 1000)
